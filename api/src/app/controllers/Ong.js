f47003dc4caab3bede0d1dba8b48ba5d2ea54f7a
otoniricardo <otoniricardo@gmail.com>
2020-03-31 12:57:57 -0300

    Funcionalidades avançadas - Implementa teste unitario

--- api/src/app/controllers/Ong.js
+++ api/src/app/controllers/Ong.js
@@ -1,10 +1,12 @@
-import crypto from 'crypto';
 import connection from '../../database/connection';
+
+import generateUniqueId from '../../utils/generateUniqueId';
 
 class OngController {
   async store(req, res) {
     const { name, email, whatsapp, city, uf } = req.body;
-    const id = crypto.randomBytes(4).toString('HEX');
+
+    const id = generateUniqueId();
 
     await connection('ongs').insert({
       id,
