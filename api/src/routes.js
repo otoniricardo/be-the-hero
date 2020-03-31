b144b92f775665d73e748f481939caf4a52a0feb
otoniricardo <otoniricardo@gmail.com>
2020-03-31 11:48:29 -0300

    Ajuste rota /incidents para mobile

--- api/src/routes.js
+++ api/src/routes.js
@@ -14,10 +14,10 @@ routes.post('/sessions', SessionController.store);
 routes.post('/ongs', OngController.store);
 routes.get('/ongs', OngController.index);
 
+routes.get('/incidents', IncidentController.index);
 routes.use(authMiddleware);
 
 routes.post('/incidents', IncidentController.store);
-routes.get('/incidents', IncidentController.index);
 routes.delete('/incidents/:id', IncidentController.delete);
 
 routes.get('/profile', ProfileController.index);
